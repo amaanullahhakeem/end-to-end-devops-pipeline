e4b7612ae9bdedd218e214887329bad3d4df4f78
Ubuntu <ubuntu@ip-172-31-45-74.ap-south-1.compute.internal>
2026-03-24 04:56:22 +0000

    all set

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("DevOps Pipeline v1 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
